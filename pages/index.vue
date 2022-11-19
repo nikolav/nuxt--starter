c0f6c0534cf24307f90837419c4dd78c78c90b86
nikolav <admin@nikolav.rs>
2022-11-19 04:32:24 +0000

    Sat Nov 19 05:32:17 CET 2022

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
+// @@
 const userRegister = async () => {
   const user = await $fetch("/api/auth/register", {
-    method: "post", 
+    method: "post",
     body: {
-      email: "user4@nikolav.rs",
-      password: "122333"
-    }
+      email: "admin@nikolav.rs",
+      password: "122333",
+    },
   });
   console.log({ user });
-}
+};
+// @@
+const userLogin = async () => {
+  const auth = await $fetch("/api/auth/authenticate", {
+    method: "post",
+    body: {
+      email: "admin@nikolav.rs",
+      password: "122333",
+    },
+  });
+  console.log({ auth });
+};
 </script>
 
 <template>
-<h1>
-  auth
-</h1>
-<v-btn variant="outlined" @click="userRegister">
-  register
-</v-btn>
+  <h1>auth</h1>
+  <v-btn variant="outlined" @click="userRegister"> register </v-btn>
+  <v-btn variant="outlined" @click="userLogin"> login </v-btn>
 </template>
 
 <style scoped>
